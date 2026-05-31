d235618d48fd77a857c6af253878495aa4f65478
Costin Hateganu <costin.hateganu@gmail.com>
2026-05-31 11:47:34 +0300

    MILESTONE-0008: Implement FiscalRuleRepository.FindById

--- src/FiscalOS.LegalKnowledge/FiscalRuleRepository.cs
+++ src/FiscalOS.LegalKnowledge/FiscalRuleRepository.cs
@@ -3,12 +3,19 @@ namespace FiscalOS.LegalKnowledge;
 public sealed class FiscalRuleRepository
 {
     private readonly List<FiscalRule> _rules = [];
+
     public void Add(FiscalRule rule)
-{
-    _rules.Add(rule);
-}   
+    {
+        _rules.Add(rule);
+    }
+
     public IReadOnlyCollection<FiscalRule> GetAll()
-{
-    return _rules.AsReadOnly();
-}
+    {
+        return _rules.AsReadOnly();
+    }
+
+    public FiscalRule? FindById(Guid id)
+    {
+        return _rules.FirstOrDefault(x => x.Id == id);
+    }
 }
