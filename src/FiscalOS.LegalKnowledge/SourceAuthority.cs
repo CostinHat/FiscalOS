6a9f7a6e734ed4544e63412c3aacba653dad99c3
Costin Hateganu <costin.hateganu@gmail.com>
2026-06-01 19:22:56 +0300

    FOS-0034 Map LegalSourceType to SourceAuthorityLevel

---
+++ src/FiscalOS.LegalKnowledge/SourceAuthority.cs
@@ -0,0 +1,17 @@
+namespace FiscalOS.LegalKnowledge;
+
+public static class SourceAuthority
+{
+    public static SourceAuthorityLevel LevelFor(LegalSourceType sourceType) =>
+        sourceType switch
+        {
+            LegalSourceType.Law                => SourceAuthorityLevel.Statutory,
+            LegalSourceType.EmergencyOrdinance => SourceAuthorityLevel.Statutory,
+            LegalSourceType.FiscalCode         => SourceAuthorityLevel.Statutory,
+            LegalSourceType.GovernmentDecision => SourceAuthorityLevel.Government,
+            LegalSourceType.MethodologicalNorm => SourceAuthorityLevel.Government,
+            LegalSourceType.ANAFOrder          => SourceAuthorityLevel.Administrative,
+            LegalSourceType.Other              => SourceAuthorityLevel.Unknown,
+            _ => throw new ArgumentOutOfRangeException(nameof(sourceType), sourceType, null)
+        };
+}
---
+++ tests/FiscalOS.VerticalSliceTests/Should_Map_Source_Type_To_Authority_Level.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FiscalOS.LegalKnowledge;
+using Xunit;
+
+namespace FiscalOS.VerticalSliceTests;
+
+public sealed class Should_Map_Source_Type_To_Authority_Level
+{
+    private static readonly IReadOnlyDictionary<LegalSourceType, SourceAuthorityLevel> Expected =
+        new Dictionary<LegalSourceType, SourceAuthorityLevel>
+        {
+            [LegalSourceType.Law] = SourceAuthorityLevel.Statutory,
+            [LegalSourceType.EmergencyOrdinance] = SourceAuthorityLevel.Statutory,
+            [LegalSourceType.FiscalCode] = SourceAuthorityLevel.Statutory,
+            [LegalSourceType.GovernmentDecision] = SourceAuthorityLevel.Government,
+            [LegalSourceType.MethodologicalNorm] = SourceAuthorityLevel.Government,
+            [LegalSourceType.ANAFOrder] = SourceAuthorityLevel.Administrative,
+            [LegalSourceType.Other] = SourceAuthorityLevel.Unknown
+        };
+
+    [Theory]
+    [InlineData(LegalSourceType.Law, SourceAuthorityLevel.Statutory)]
+    [InlineData(LegalSourceType.EmergencyOrdinance, SourceAuthorityLevel.Statutory)]
+    [InlineData(LegalSourceType.FiscalCode, SourceAuthorityLevel.Statutory)]
+    [InlineData(LegalSourceType.GovernmentDecision, SourceAuthorityLevel.Government)]
+    [InlineData(LegalSourceType.MethodologicalNorm, SourceAuthorityLevel.Government)]
+    [InlineData(LegalSourceType.ANAFOrder, SourceAuthorityLevel.Administrative)]
+    [InlineData(LegalSourceType.Other, SourceAuthorityLevel.Unknown)]
+    public void Maps_each_source_type_to_its_authority_level(
+        LegalSourceType sourceType,
+        SourceAuthorityLevel expected)
+    {
+        Assert.Equal(expected, SourceAuthority.LevelFor(sourceType));
+    }
+
+    [Fact]
+    public void Every_declared_source_type_is_mapped_as_expected()
+    {
+        foreach (var sourceType in Enum.GetValues<LegalSourceType>())
+        {
+            Assert.True(
+                Expected.ContainsKey(sourceType),
+                $"LegalSourceType.{sourceType} has no expected mapping defined.");
+
+            Assert.Equal(Expected[sourceType], SourceAuthority.LevelFor(sourceType));
+        }
+    }
+
+    [Fact]
+    public void Never_maps_a_declared_source_type_to_constitutional()
+    {
+        var levels = Enum.GetValues<LegalSourceType>()
+            .Select(SourceAuthority.LevelFor);
+
+        Assert.DoesNotContain(SourceAuthorityLevel.Constitutional, levels);
+    }
+
+    [Fact]
+    public void Throws_for_an_undefined_source_type()
+    {
+        var undefined = (LegalSourceType)999;
+
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => SourceAuthority.LevelFor(undefined));
+    }
+}
